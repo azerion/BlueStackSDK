253f194a9d484a952ecc692c6d72a877be8f04f9
MedAnypli <mebensalem@anypli.com>
2022-11-22 08:35:27 +0100

    #v4.1.7 : push the frameworks of the blueStackSDK .

---
+++ BlueStackImproveAdapter.xcframework/ios-arm64/BlueStackImproveAdapter.framework/Headers/BlueStackImproveAdapter.h
@@ -0,0 +1,18 @@
+//
+//  BlueStackImproveAdapter.h
+//  BlueStackImproveAdapter
+//
+//  Created by MedSghaier on 8/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BlueStackImproveAdapter.
+FOUNDATION_EXPORT double BlueStackImproveAdapterVersionNumber;
+
+//! Project version string for BlueStackImproveAdapter.
+FOUNDATION_EXPORT const unsigned char BlueStackImproveAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlueStackImproveAdapter/PublicHeader.h>
+
+
