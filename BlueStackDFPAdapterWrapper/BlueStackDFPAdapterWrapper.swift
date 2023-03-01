5043d11ae62833b57feb81b8e196655661cbe586
MedAnypli <mebensalem@anypli.com>
2023-03-01 10:20:25 +0100

    add new path to support target binairies

---
+++ BlueStackDFPAdapterWrapper/BlueStackDFPAdapterWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by MedSghaier on 21/2/2023.
+//
+
+import Foundation
---
+++ BlueStackSDKWrapper/BlueStackSDKWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by MedSghaier on 16/2/2023.
+//
+
+import Foundation
