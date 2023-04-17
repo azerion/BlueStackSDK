567d430178cc96c9c723d626917e6f7315d9d2b5
Torongo <torongo.azad@vivacomsolutions.com>
2023-04-17 15:46:37 +0600

    Adding google utilities as dependency

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "GoogleUtilities", url: "https://github.com/google/GoogleUtilities", "7.4.3"..<"7.11.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -76,8 +77,8 @@ let package = Package(
                ),
         .target(name: "BlueStackDFPAdapterWrapper",
                 dependencies: [
+                    .product(name: "MyGoogleUtilities", package: "GoogleUtilities"),
                     .target(name: "MyFBLPromises", condition: .when(platforms: [.iOS])),
-                    .target(name: "MyGoogleUtilities", condition: .when(platforms: [.iOS])),
                     .target(name: "Mynanopb", condition: .when(platforms: [.iOS])),
                      .target(name: "BlueStackDFPAdapter", condition: .when(platforms: [.iOS])),
                 ],
