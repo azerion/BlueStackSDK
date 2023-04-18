b245f9f33f570d083fb941cb9d8714e7a244faeb
Torongo <torongo.azad@vivacomsolutions.com>
2023-04-18 13:49:39 +0600

    remove dynamic

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BlueStackSDK",
-            type: .dynamic,
-            targets: ["BlueStackSDKWrapper"]),
+            targets: ["BlueStackSDKWrapper"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
