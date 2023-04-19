fbd35084f78ad65919c4857b1bc197dce7aa4537
Torongo <torongo.azad@vivacomsolutions.com>
2023-04-19 12:46:29 +0600

    promises removed

--- Package.swift
+++ Package.swift
@@ -15,8 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads", "9.0.0"..<"10.3.0"),
-        .package(url: "https://github.com/google/promises", "1.1.0"..<"2.0.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads", .upToNextMajor(from: "9.13.0")),
         
     ],
     targets: [
@@ -78,7 +77,6 @@ let package = Package(
                 dependencies: [
                     .target(name: "BlueStackDFPAdapter", condition: .when(platforms: [.iOS])),
                     .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
-                    .product(name: "FBLPromises", package: "promises"),
                 ],
                 path: "BlueStackDFPAdapterWrapper"
                ),
