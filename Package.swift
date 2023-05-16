fc58aa4c1a085d3cc12f611cdda0b5958fdef573
Torongo <torongo.azad@vivacomsolutions.com>
2023-05-16 17:42:44 +0600

    version updated

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
             targets: ["BlueStackOguryAdapterTarget"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads", .upToNextMajor(from: "9.12.0")),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads", .upToNextMajor(from: "9.13.0")),
         .package(url: "https://github.com/azerion/improvedigital-sdk-ios", .upToNextMajor(from: "2.0.1")),
     ],
     targets: [
