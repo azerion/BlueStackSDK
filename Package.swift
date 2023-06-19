3c37ba8b5f284d6ab29aa9c89e1b235485ad10bf
Torongo <torongo.azad@vivacomsolutions.com>
2023-06-19 20:38:47 +0600

    BlueStackSDK and adapters updated

--- Package.swift
+++ Package.swift
@@ -40,7 +40,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads", .upToNextMajor(from: "9.13.0")),
-        .package(url: "https://github.com/azerion/improvedigital-sdk-ios", .upToNextMajor(from: "2.0.2")),
+        .package(url: "https://github.com/azerion/improvedigital-sdk-ios", .upToNextMajor(from: "3.0.0-preview.1")),
     ],
     targets: [
         .target(name: "BlueStackDFPAdapterTarget",
