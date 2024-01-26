43ee05e77237f48fb1b686370014a4be66e06fd6
Torongo <torongo.azad@vivacomsolutions.com>
2024-01-26 12:20:48 +0600

    Updated BlueStackSDK

---
+++ BlueStackSDK.xcframework/ios-arm64/BlueStackSDK.framework/Headers/MNGNativeAdImpressionListener.h
@@ -0,0 +1,14 @@
+//
+//  MNGNativeAdImpressionListener.h
+//  BlueStackSDK
+//
+//  Created by Nagib Bin Azad on 23/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MNGNativeAdImpressionListener <NSObject>
+
+- (void)onAdImpression;
+
+@end
