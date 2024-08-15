7b84e4ce4d7b4223a41b33cc5a75c9016c67e957
Torongo <torongo.azad@vivacomsolutions.com>
2024-08-15 20:04:19 +0600

    updated smart adapter added

--- BlueStackSASAdapter.xcframework/ios-arm64_x86_64-simulator/BlueStackSASAdapter.framework/Headers/BlueStackSASAdapter.h
+++ BlueStackSASAdapter.xcframework/ios-arm64_x86_64-simulator/BlueStackSASAdapter.framework/Headers/BlueStackSASAdapter.h
@@ -2,17 +2,11 @@
 //  BlueStackSASAdapter.h
 //  BlueStackSASAdapter
 //
-//  Created by Hussein Dimessi on 11/2/20.
+//  Created by Nagib Bin Azad on 24/4/24.
 //
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for BlueStackSASAdapter.
-FOUNDATION_EXPORT double BlueStackSASAdapterVersionNumber;
+@interface BlueStackSASAdapter : NSObject
 
-//! Project version string for BlueStackSASAdapter.
-FOUNDATION_EXPORT const unsigned char BlueStackSASAdapterVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <BlueStackSASAdapter/PublicHeader.h>
-
-
+@end
