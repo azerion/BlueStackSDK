f6f666e1978b676750b9c08754952c4560b3c093
Torongo <torongo.azad@vivacomsolutions.com>
2024-08-15 20:06:05 +0600

    smart bidding adapter added

---
+++ BlueStackSASBiddingAdapter.xcframework/ios-arm64_x86_64-simulator/BlueStackSASBiddingAdapter.framework/Headers/BlueStackSASBiddingAdapter.h
@@ -0,0 +1,12 @@
+//
+//  BlueStackSASBiddingAdapter.h
+//  BlueStackSASBiddingAdapter
+//
+//  Created by Nagib Bin Azad on 25/4/24.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BlueStackSASBiddingAdapter : NSObject
+
+@end
