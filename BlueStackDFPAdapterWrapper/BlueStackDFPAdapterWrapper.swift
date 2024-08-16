3d4ab9950fd8a8061f223598448e684e1ac291e7
Torongo <torongo.azad@vivacomsolutions.com>
2024-08-16 19:24:01 +0600

    Removed DFP and added new GMA adapter

--- BlueStackDFPAdapterWrapper/BlueStackDFPAdapterWrapper.swift
+++
@@ -1,8 +0,0 @@
-//
-//  BlueStackDFPAdapterWrapper.swift
-//  
-//
-//  Created by MedSghaier on 21/2/2023.
-//
-
-import Foundation
---
+++ BlueStackGMAAdapterWrapper/BlueStackGMAAdapterWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  BlueStackGMAAdapterWrapper.swift
+//
+//
+//  Created by Nagib Bin Azad on 26/4/23.
+//
+
+import Foundation
