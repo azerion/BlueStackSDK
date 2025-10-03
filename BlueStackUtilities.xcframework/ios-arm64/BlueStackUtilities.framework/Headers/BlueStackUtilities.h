137a32370f8dc3d33f0088cd8a270f9ad4b78a40
Torongo <torongo.azad@vivacomsolutions.com>
2025-10-03 15:38:18 +0600

    Utility framework added

---
+++ BlueStackUtilities.xcframework/ios-arm64/BlueStackUtilities.framework/Headers/BlueStackUtilities.h
@@ -0,0 +1,18 @@
+//
+//  BlueStackUtilities.h
+//  BlueStackUtilities
+//
+//  Created by Nagib Bin Azad on 12/8/25.
+//
+
+#import <Foundation/Foundation.h>
+#import <BlueStackUtilities/BLSLogger.h>
+//! Project version number for BlueStackUtilities.
+FOUNDATION_EXPORT double BlueStackUtilitiesVersionNumber;
+
+//! Project version string for BlueStackUtilities.
+FOUNDATION_EXPORT const unsigned char BlueStackUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlueStackUtilities/PublicHeader.h>
+
+
